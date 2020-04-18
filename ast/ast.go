7bf79bb3efff69d8593a55e7569a73b69ffb8c73
pocari <caffelattenonsugar@gmail.com>
2020-04-18 15:00:07 +0900

    IntegerLiteral追加

--- ast/ast.go
+++ ast/ast.go
@@ -50,7 +50,6 @@ func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
-
 func (i *Identifier) String() string {
 	return i.Value
 }
@@ -120,4 +119,17 @@ func (es *ExpressionStatement) String() string {
 	}
 
 	return ""
+}
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode() {}
+func (il *IntegerLiteral) TokenLiteral() string {
+	return il.Token.Literal
+}
+func (il *IntegerLiteral) String() string {
+	return string(il.Value)
 }
