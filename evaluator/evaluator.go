27c7f0737a9000a5a4719a1e01d48db7a92941c4
pocari <caffelattenonsugar@gmail.com>
2020-04-19 12:34:08 +0900

    if文の評価追加

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -30,6 +30,10 @@ func Eval(node ast.Node) object.Object {
 		left := Eval(node.Left)
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 	}
 	return nil
 }
@@ -121,5 +125,29 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 		return nativeBoolToBooleanObject(l != r)
 	default:
 		return NULL
+	}
+}
+
+func evalIfExpression(node *ast.IfExpression) object.Object {
+	condition := Eval(node.Condition)
+
+	if isTruthy(condition) {
+		return Eval(node.Consequence)
+	} else {
+		if node.Alternative != nil {
+			return Eval(node.Alternative)
+		}
+	}
+	return NULL
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case FALSE:
+		return false
+	default:
+		return true
 	}
 }
