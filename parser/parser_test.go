5b7392a70591bef1b7b7c039524f405c7dc734d4
pocari <caffelattenonsugar@gmail.com>
2020-04-19 10:12:35 +0900

    関数呼び出し用のastとテスト追加

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -696,3 +696,54 @@ func TestFunctionParameterParsing(t *testing.T) {
 		}
 	}
 }
+
+func TestCallExpressionParsing(t *testing.T) {
+	input := `add(1, 2 * 3, 4 + 5)`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParseError(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. got=%d",
+			len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+			program.Statements[0])
+	}
+
+	exp, ok := stmt.Expression.(*ast.CallExpression)
+	if !ok {
+		t.Fatalf("exp not *ast.CallExpression. got=%T", stmt.Expression)
+	}
+
+	if len(exp.Arguments) != 3 {
+		t.Fatalf("exp.Arguments has not enough Arguments. got=%d",
+			len(exp.Arguments))
+	}
+
+	if !testIdentifier(t, exp.Function, "add") {
+		return
+	}
+
+	if len(exp.Arguments) != 3 {
+		t.Fatalf("len(exp.Arguments) should have  %d parameters. but got=%T",
+			3, len(exp.Arguments))
+	}
+
+	if !testLiteralExpression(t, exp.Arguments[0], 1) {
+		return
+	}
+
+	if !testInfixExpression(t, exp.Arguments[1], 2, "*", 3) {
+		return
+	}
+
+	if !testInfixExpression(t, exp.Arguments[2], 4, "+", 5) {
+		return
+	}
+}
